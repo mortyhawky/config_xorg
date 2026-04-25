dd5a6d45b3553dcd3f90548acf4e017745292ea6
mortyhawky <morty.hawky@outlook.com>
2026-04-25 14:04:12 +0200

    ArchReal:

--- dwm_status.sh
+++ dwm_status.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
+## Updated ArchReal 2026-04-25 11:32 Sat
 
 while true; do
-  _day=$(date +"%a")
   _date=$(date -I)
   _time=$(date +"%H:%M")
+  _day=$(date +"%a")
   xsetroot -name " $_date $_time $_day "
   sleep 5
 done
